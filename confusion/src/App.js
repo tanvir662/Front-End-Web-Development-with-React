dc83989d1f10ed7a11b9e26a3d5fba6b60bcb4a0
Tanvir <43845829+tanvir662@users.noreply.github.com>
2020-05-31 20:26:03 +0600

    First Assignment

---
+++ confusion/src/App.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import logo from './logo.svg';
+import {Component} from 'react';
+import Menu from './components/MenuComponent';
+import { Navbar, NavbarBrand } from 'reactstrap';
+import './App.css';
+import { DISHES } from './shared/dishes';
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      dishes: DISHES
+    };
+  }
+
+render(){
+    return (
+      <div >
+          <Navbar dark color="primary">
+            <div className="container">
+              <NavbarBrand href="/">Restaurant ConFusion</NavbarBrand>
+            </div>
+          </Navbar>
+          <Menu dishes={this.state.dishes} />
+        
+      </div>
+    );
+  }
+}
+export default App;
