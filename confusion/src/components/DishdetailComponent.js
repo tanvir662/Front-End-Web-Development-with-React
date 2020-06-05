88b3608f840edf15db6b4dcd9afb39d520d6ed67
Tanvir <43845829+tanvir662@users.noreply.github.com>
2020-06-06 02:16:27 +0600

    2nd Assignment

--- confusion/src/components/DishdetailComponent.js
+++ confusion/src/components/DishdetailComponent.js
@@ -1,5 +1,7 @@
 import React, { Component } from 'react';
 import { Card, CardImg, CardBody, CardText, CardTitle } from 'reactstrap';
+import {Breadcrumb, BreadcrumbItem } from 'reactstrap';
+import { Link } from 'react-router-dom';
 
 class Dishdetail extends Component {
 
@@ -23,12 +25,16 @@ class Dishdetail extends Component {
             )
         })
         return (
-            <div className="col-12 col-md-5 m-1">
-                <h4> Comments </h4>
-                <ul className='list-unstyled'>
-                    {cmnts}
-                </ul>
+            <div className="container">
+                <div className="row">
+                    <div className="col-md-4 ml-auto">
+                        <h4> Comments </h4>
+                        <ul className='list-unstyled'>
+                            {cmnts}
+                        </ul>
 
+                    </div>
+                </div>
             </div>
         )
     }
@@ -36,14 +42,31 @@ class Dishdetail extends Component {
     renderDish(dish) {
         if (dish != null) {
             return (
-                <div className="col-12 col-md-5 m-1">
-                    <Card>
-                        <CardImg  src={dish.image} alt={dish.name} />
-                        <CardBody>
-                            <CardTitle>{dish.name}</CardTitle>
-                            <CardText>{dish.description}</CardText>
-                        </CardBody>
-                    </Card>
+                <div className="container">
+                    <div className="row">
+                    <Breadcrumb>
+
+                        <BreadcrumbItem><Link to="/menu">Menu</Link></BreadcrumbItem>
+                        <BreadcrumbItem active>{dish.name}</BreadcrumbItem>
+                    </Breadcrumb>
+                        <div className="col-12">
+                            <h3>{dish.name}</h3>
+                            <hr />
+                        </div>                
+                    </div>
+                
+                    <div className="col-12 col-md-5 m-1">
+                        <Card>
+                            <CardImg  src={dish.image} alt={dish.name} />
+                            <CardBody>
+                                <CardTitle>{dish.name}</CardTitle>
+                                <CardText>{dish.description}</CardText>
+                        
+                            </CardBody>
+                        </Card>
+                        
+
+                    </div>
                 </div>
             )
         }
@@ -54,11 +77,13 @@ class Dishdetail extends Component {
 
     render() {
         const dish = this.props.dish
+        const comments = this.props.comments
+        
         if (dish == null) {
             return (<div></div>)
         }
         const dishItem = this.renderDish(dish)
-        const commentItem = this.renderComments(dish.comments)
+        const commentItem = this.renderComments(comments)
         return (
             <div className="container">
                 <div className='row'>
--- confusion/src/components/MainComponent.js
+++ confusion/src/components/MainComponent.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import Menu from './MenuComponent';
 import { DISHES } from '../shared/dishes';
 import Header from './HeaderComponent';
+import Dishdetail from './DishdetailComponent';
 import Footer from './FooterComponent';
 import Home from './HomeComponent';
 import { Switch, Route, Redirect } from 'react-router-dom';
@@ -9,6 +10,7 @@ import Contact from './ContactComponent';
 import { COMMENTS } from '../shared/comments';
 import { PROMOTIONS } from '../shared/promotions';
 import { LEADERS } from '../shared/leaders';
+import About from './AboutComponent';
 
 class Main extends Component {
 
@@ -32,13 +34,21 @@ class Main extends Component {
           />
       );
     }
+    const DishWithId = ({match}) => {
+      return(
+          <Dishdetail dish={this.state.dishes.filter((dish) => dish.id === parseInt(match.params.dishId,10))[0]} 
+            comments={this.state.comments.filter((comment) => comment.dishId === parseInt(match.params.dishId,10))} />
+      );
+    };
     return (
       <div>
         <Header/>
         <Switch>
               <Route path='/home' component={HomePage} />
               <Route exact path='/menu' component={() => <Menu dishes={this.state.dishes} />} />
-              <Route exact path='/contactus' component={Contact} />} />
+              <Route path='/menu/:dishId' component={DishWithId} />
+              <Route exact path='/contactus' component={Contact} /> />
+               <Route path='/aboutus' component={() => <About leaders={this.state.leaders} />}/>
               <Redirect to="/home" />
         </Switch>
         <Footer/>
