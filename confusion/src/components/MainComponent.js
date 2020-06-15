ef2a82d862720a942809d8cf53120784788145ad
Tanvir <43845829+tanvir662@users.noreply.github.com>
2020-06-15 23:56:04 +0600

    React Redux Forms Revisited

--- confusion/src/components/MainComponent.js
+++ confusion/src/components/MainComponent.js
@@ -9,7 +9,7 @@ import Contact from './ContactComponent';
 import About from './AboutComponent';
 import { connect } from 'react-redux';
 import { addComment, fetchDishes } from '../redux/ActionCreators';
-
+import { actions } from 'react-redux-form';
 const mapStateToProps = state => {
   return {
     dishes: state.dishes,
@@ -22,7 +22,8 @@ const mapStateToProps = state => {
 const mapDispatchToProps = dispatch => ({
   
   addComment: (dishId, rating, author, comment) => dispatch(addComment(dishId, rating, author, comment)),
-  fetchDishes: () => { dispatch(fetchDishes())}
+  fetchDishes: () => { dispatch(fetchDishes())},
+  resetFeedbackForm: () => { dispatch(actions.reset('feedback'))}
 });
 
 
@@ -62,7 +63,7 @@ class Main extends Component {
               <Route path='/home' component={HomePage} />
               <Route exact path='/menu' component={() => <Menu dishes={this.props.dishes} />} />
               <Route path='/menu/:dishId' component={DishWithId} />
-              <Route exact path='/contactus' component={Contact} /> 
+              <Route exact path='/contactus' component={() => <Contact resetFeedbackForm={this.props.resetFeedbackForm} />} />
                <Route path='/aboutus' component={() => <About leaders={this.props.leaders} />}/>
               <Redirect to="/home" />
         </Switch>
